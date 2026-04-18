89d87b1f375121ff3d5b24ff075fb614052e4385
Ronald <ronaldveliz1004@gmail.com>
2026-04-17 23:28:01 -0400

    Initial commands planted

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ from scripts.read_data import *
 Gids = json.load(open("data/gids.json"))
 
 def GetData(Tittle):
-    try:
-        df = pd.read_csv(URL.replace('[GID]',Gids[Tittle]))
-        return df.to_numpy()
-    
-    except Exception as e:
-        return f"Error: {e}"
+    print(Tittle)
+    df = pd.read_csv(URL.replace('[GID]',Gids[Tittle]))
+    return df.to_numpy()
 
 if __name__ == "__main__":
     while True:
-        res = input('- ')
+        flags = False
+
+        res = input('> ')
+        UserInput = res.split()
+
+        if "--" in res:
+            flags = True
+
+        match UserInput[0]:
+            case "help":
+                pass
+            case "check":
+                pass
+            case "":
+                pass
 
